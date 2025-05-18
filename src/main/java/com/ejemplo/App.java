6b90ff4db945fa7d546775569edb03bc90bd5dc8
laura ladino <162608228+githublauraa@users.noreply.github.com>
2025-05-18 11:20:59 -0500

    Update App.java

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -6,8 +6,16 @@ package com.ejemplo;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Demostración de la aplicación");
+        
+        Calculadora calc = new Calculadora();
+        System.out.println("Suma de 5 + 3 = " + calc.sumar(5, 3));
+        
+        Validador val = new Validador();
+        System.out.println("¿Es 4 un número par? " + val.esPar(4));
+        
+        Conversor conv = new Conversor();
+        System.out.println("25°C en Fahrenheit es: " + conv.celsiusAFahrenheit(25));
     }
 }
